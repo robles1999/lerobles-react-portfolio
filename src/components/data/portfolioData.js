1bcd91bcd01510d937cfed45647735accf3bc03f
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-06-12 10:21:55 -0400

    Superher Showdown project added.

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -1,4 +1,5 @@
-import heartfelt from '../../images/heartfelt.jpg'
+import superheroShowdown from '../../images/superhero-showdown.png';
+import heartfelt from '../../images/heartfelt.jpg';
 import playerOneReads from '../../images/player-one-reads.png';
 import techBlog from "../../images/tech-blog.jpg";
 import WeatherApp from "../../images/weather-app.jpg";
@@ -7,46 +8,53 @@ import passwordGenerator from "../../images/password-generator.png";
 
 const portfolioData = [
   {
+    title: "SuperHero Showdown",
+    gitHub: "https://github.com/Suzakijun1/Higher-Lower-Game",
+    deployed: "https://sleepy-shore-92951.herokuapp.com/",
+    image: superheroShowdown,
+    id: 0,
+  },
+  {
     title: "HeartfeltRSVP",
     gitHub: "https://github.com/MikeK1639/Heartfelt-RSVP",
     deployed: "https://heartrsvp.herokuapp.com/login",
     image: heartfelt,
-    id: 0,
+    id: 1,
   },
   {
     title: "Player 1 Reads",
     gitHub: "https://github.com/EGARRISXN/Player-One-Reads",
     deployed: "https://egarrisxn.github.io/Player-One-Reads/",
     image: playerOneReads,
-    id: 1,
+    id: 2,
   },
   {
     title: "Tech Blog",
     gitHub: "https://github.com/robles1999/lerobles-tech-blog",
     deployed: "https://lerobles-tech-blog.herokuapp.com/",
     image: techBlog,
-    id: 2,
+    id: 3,
   },
   {
     title: "Weather Dashboard",
     gitHub: "https://github.com/robles1999/weather-forecast-module-6",
     deployed: "https://robles1999.github.io/weather-forecast-module-6/",
     image: WeatherApp,
-    id: 3,
+    id: 4,
   },
   {
     title: "Code Quiz",
     gitHub: "https://github.com/robles1999/js-quiz-game",
     deployed: "https://robles1999.github.io/js-quiz-game/",
     image: CodeQuiz,
-    id: 4,
+    id: 5,
   },
   {
     title: "Password Generator",
     gitHub: "https://github.com/robles1999/password-challenge-module-3",
     deployed: "https://robles1999.github.io/password-challenge-module-3/",
     image: passwordGenerator,
-    id: 4,
+    id: 6,
   },
 ];
 
