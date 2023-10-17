fefdc79874be15e73db72dde535f60947e52358a
Luis Robles <robles1999@yahoo.com>
2023-10-17 14:21:21 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,17 @@ import "../../src/App.css";
 // ::::: Set the Font Awesome icons :::::
 // ::::: EMAIL
 const mailIcon = (
-  <FontAwesomeIcon icon={faEnvelope} size="2xl" style={{ color: "black" }} />
+  <FontAwesomeIcon icon={faEnvelope} size="2xl" style={{ color: "gray" }} />
 );
 
 // ::::: GITHUB
 const gitHub = (
-  <FontAwesomeIcon icon={faGithub} size="2xl" style={{ color: "black" }} />
+  <FontAwesomeIcon icon={faGithub} size="2xl" style={{ color: "gray" }} />
 );
 
 // ::::: LINKED-IN
 const linkedIn = (
-  <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{ color: "black" }} />
+  <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{ color: "gray" }} />
 );
 
 export default function Footer() {
@@ -25,22 +25,22 @@ export default function Footer() {
     <div className="fixed-bottom footer ">
       <div>
         <ul className="nav justify-content-center ">
-          <div className="nav justify-content-center bg-secondary rounded">
+          <div className="nav justify-content-center bg-dark rounded">
           <li className="nav-item p-3">
             {/* SET EMAIL LINK */}
-            <a className="border-left" href="mailto: robles1999@gmail.com" target="_blank">
+            <a href="mailto: robles1999@gmail.com" target="_blank">
               {mailIcon}
             </a>
           </li>
           {/* SET GITHUB LINK */}
           <li className="nav-item p-3">
-            <a href="https://github.com/robles1999" target="_blank">
+              <a href="https://github.com/robles1999" target="_blank">
               {gitHub}
             </a>
           </li>
           <li className="nav-item p-3">
             {/* SET LINKED-IN LINK */}
-            <a
+              <a
               href="https://www.linkedin.com/in/luis-enrique-robles/"
               target="_blank"
             >
